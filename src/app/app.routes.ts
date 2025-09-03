b36e96566f5091c78887f4f9dd50d1993dbd0eea
Joe Chung <joechung2008@users.noreply.github.com>
2025-09-02 23:22:30 -0400

    Lazy load AppComponent to improve bundle sizes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,11 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from '../components/App/App.component';
 
-export const routes: Routes = [{ path: '', component: AppComponent }];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: async () => {
+      const { AppComponent } = await import('../components/App/App.component');
+      return AppComponent;
+    },
+  },
+];
